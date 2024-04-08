b1ed97bb2c851a302a6fbf251f7d87c9bbc75d0f
darrylbridges <bridgesjr92@gmail.com>
2024-04-07 22:14:39 -0700

    added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(x)
+
+    {
+    let newname = [...x]
+    let message = [new Array(newname.length)]
+
+    for (let i = 0; i < newname.length; i++ )
+    {
+        message[i] = `Thank you, ${newname[i]}, for the wonderful surprise gift!`;
+    }
+     return message
+}
+
+
+function countDown (x) {
+for (let i = x; i >= 0; i--)
+{
+    console.log(i)
+    }
+}
